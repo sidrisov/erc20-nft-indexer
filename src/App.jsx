7628f97e6ba1aee5a6363af61b9293573541b6cb
Sinaver Idris <sinavercrimea@gmail.com>
2023-01-13 16:00:24 +0400

    added layout separation to differentiate nft/erc20 tokens

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,8 @@ import {
   Avatar,
   Box,
   CircularProgress,
-  Skeleton
+  Skeleton,
+  Container
 } from '@mui/material';
 
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
@@ -43,12 +44,16 @@ const theme = createTheme({
 
 const ALCHEMY_API_KEY = import.meta.env.VITE_ALCHEMY_TESTNET_API_KEY;
 
+
 function App() {
   const [connected, setConnected] = useState(false);
   const [userAddress, setUserAddress] = useState("");
   const [results, setResults] = useState([]);
   const [hasQueried, setHasQueried] = useState(false);
   const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [tokenFilter, setTokenFilter] = useState({
+    "isErc20": true // false for nfts
+  });
 
   async function connectWallet() {
     const accounts = await provider.send('eth_requestAccounts', []);
@@ -73,6 +78,13 @@ function App() {
     setResults([]);
   }
 
+  async function filterByTokenType() {
+    const isErc20 = !tokenFilter.isErc20;
+    setTokenFilter({ ...tokenFilter, isErc20 });
+
+    console.log(tokenFilter);
+  }
+
   async function getTokenBalance() {
     const config = {
       apiKey: ALCHEMY_API_KEY,
@@ -95,65 +107,71 @@ function App() {
     setHasQueried(true);
   }
   return (
-      <ThemeProvider theme={theme}>
-        <AppBar position='fixed' color='primary'>
-          <Toolbar variant='dense'>
-            <DownloadingIcon color={connected ? "secondary" : "inherit"} />
-            <Typography
-              color={connected ? "secondary" : "inherit"}
-              variant='h6'
-              sx={{ flexGrow: 1 }}
-            >
-              Indexer
-            </Typography>
-            {connected &&
-              <Stack direction="row" spacing={1}>
-                {
-                  !hasQueried && <CircularProgress color="secondary" size={25}
-                    thickness={4} />
-                }
-                <Chip
-                  label={userAddress}
-                  size='small'
-                  variant='filled'
-                  color='secondary'
-                  deleteIcon={<PowerSettingsNewIcon />}
-                  onDelete={() => {
-                    disconnectWallet();
-                  }}
-                />
-              </Stack>
-            }
-            {!connected && (
-              <Button
-                size='small'
-                variant='outlined'
-                color='inherit'
-                endIcon={<AccountBalanceWalletIcon />}
-                onClick={() => {
-                  connectWallet();
-                }}
-              >
-                Connect
-              </Button>
-            )}
-          </Toolbar>
-        </AppBar>
-
-        {!connected && (
-          <Typography m={30} align='center' variant='h4'>
-            “Building ERC20 & NFT Indexer Powered by Cryptographic Truth”
+    <ThemeProvider theme={theme}>
+      <AppBar position='fixed' color='primary'>
+        <Toolbar variant='dense'>
+          <DownloadingIcon color={connected ? "secondary" : "inherit"} />
+          <Typography
+            color={connected ? "secondary" : "inherit"}
+            variant='h6'
+            sx={{ flexGrow: 1 }}
+          >
+            Indexer
           </Typography>
-        )}
+          {connected &&
+            <Stack direction="row" spacing={1}>
+              {
+                !hasQueried && <CircularProgress color="secondary" size={25}
+                  thickness={4} />
+              }
 
-        {connected &&
-          <Box m={1} mt={7}>
-            <Card>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Assets
-                </Typography>
+              <Chip label="ERC20" size="small" variant="filled" color={tokenFilter.isErc20 ? "secondary" : "info"} clickable={!tokenFilter.isErc20} onClick={() => { !tokenFilter.isErc20 && filterByTokenType() }} />
+              <Chip label="NFT" size="small" variant="filled" color={tokenFilter.isErc20 ? "info" : "secondary"} clickable={tokenFilter.isErc20} onClick={() => { tokenFilter.isErc20 && filterByTokenType() }} />
 
+              <Divider orientation="vertical" flexItem sx={{ bgcolor: "white" }} />
+
+              <Chip
+                label={userAddress}
+                size='small'
+                variant='filled'
+                color='secondary'
+                deleteIcon={<PowerSettingsNewIcon />}
+                onDelete={() => {
+                  disconnectWallet();
+                }}
+              />
+            </Stack>
+          }
+          {!connected && (
+            <Button
+              size='small'
+              variant='outlined'
+              color='inherit'
+              endIcon={<AccountBalanceWalletIcon />}
+              onClick={() => {
+                connectWallet();
+              }}
+            >
+              Connect
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+
+      {!connected && (
+        <Typography m={30} align='center' variant='h4'>
+          “Building ERC20 & NFT Indexer Powered by Cryptographic Truth”
+        </Typography>
+      )}
+
+      {connected &&
+        <Box m={1} mt={7} sx={{ display: 'flex', flexGrow: 1 }}>
+          <Card>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Assets
+              </Typography>
+              {tokenFilter.isErc20 ? (
                 <TableContainer>
                   <Table sx={{ minWidth: 650 }}>
                     <TableHead>
@@ -190,21 +208,22 @@ function App() {
                     </TableBody>
                   </Table>
                 </TableContainer>
-                {
-                  !hasQueried &&
-                  <Stack>
-                    <Skeleton variant="text" height={75} />
-                    <Divider></Divider>
-                    <Skeleton variant="text" height={75} />
-                    <Divider></Divider>
-                    <Skeleton variant="text" height={75} />
-                  </Stack>
-                }
-              </CardContent>
-            </Card>
-          </Box>
-        }
-      </ThemeProvider>
+              ) : "some nfts should be here"}
+              {
+                !hasQueried &&
+                <Stack>
+                  <Skeleton variant="text" height={75} />
+                  <Divider></Divider>
+                  <Skeleton variant="text" height={75} />
+                  <Divider></Divider>
+                  <Skeleton variant="text" height={75} />
+                </Stack>
+              }
+            </CardContent>
+          </Card>
+        </Box>
+      }
+    </ThemeProvider>
   );
 }
 
